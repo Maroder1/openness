ae491a9ad1acc523ab15dee928a42311f1a8ef66
dankocaboski <caboskidanko@gmail.com>
2024-05-21 16:57:58 -0300

    Feat: tabela hardware

--- Core/Openness.py
+++ Core/Openness.py
@@ -9,7 +9,7 @@ import Siemens.Engineering as tia
 project_dir = 'C:\\Users\\Willian\\Desktop\\tia_python'
 
 
-def create_project(project_path, project_name):
+def create_project(project_path, project_name, hardware):
     
     project_path = DirectoryInfo (project_dir)
     
@@ -19,8 +19,12 @@ def create_project(project_path, project_name):
 
     #Creating new project
     print ('Creating project')
+    global myproject
     myproject = mytia.Projects.Create(project_path, project_name)
 
+    for str in hardware:
+        CriaPlc(str)
+    
     #Addding Stations
     print ('Creating station 1')
     station1_mlfb = 'OrderNumber:6ES7 515-2AM01-0AB0/V2.6'
@@ -30,6 +34,13 @@ def create_project(project_path, project_name):
     station2_mlfb = 'OrderNumber:6ES7 518-4AP00-0AB0/V2.6'
     station2 = myproject.Devices.CreateWithItem(station2_mlfb, 'station2', 'station2')
 
-    print ("Press any key to quit")
-    input()
-    quit()
+    return
+
+def CriaPlc (plc_name):
+    global myproject
+    plc_mlfb = 'OrderNumber:6ES7 151-1AA05-0AB0/V2.6'
+    plc = myproject.Devices.CreateWithItem(plc_mlfb, plc_name, plc_name)
+    return
+
+############### CAMPOS ################
+myproject = None
--- Core/Screen.py
+++ Core/Screen.py
@@ -2,7 +2,7 @@ import sys
 sys.coinit_flags = 2
 import pywinauto
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, ttk 
 
 import Openness
 
@@ -28,24 +28,48 @@ def Criar():
 def open_file_dialog():
     global project_dir
     project_dir = filedialog.askdirectory()
+    
+def adicionar_linha():
+    nova_linha = {"combobox": tk.StringVar(root), "entry": tk.StringVar(root)}
+    nova_linha_frame = ttk.Frame(root)
+    
+    # Combobox na primeira coluna
+    combobox = ttk.Combobox(nova_linha_frame, textvariable=nova_linha["combobox"], values=opcoes_Hardware)
+    combobox.pack(side=tk.LEFT, padx=5)
+    
+    # Entry na segunda coluna
+    entry = ttk.Entry(nova_linha_frame, textvariable=nova_linha["entry"])
+    entry.pack(side=tk.LEFT, padx=5)
+    
+    # index_botao = botao_adicionar_linha.grid_info()
+    print(botao_adicionar_linha.grid_info())
+    nova_linha_frame.pack()
+    
+    linhas.append(nova_linha)
         
 ############### Valoriaveis ################
 
 project_dir = None
+linhas = []
+opcoes_Hardware = ["PLC", "HMI"]
 
 ############### CAMPOS ################
 
 # Nome do projeto
-label = tk.Label(root, text="Nome do projeto: ", font=("Arial", 24))
+label = tk.Label(root, text="Nome do projeto: ")
+label.pack(pady=20)  # pady é a margem vertical
 entrada1 = tk.Entry(root, textvariable = project_name_var)
 entrada1.pack()  # Adicione esta linha para exibir o campo de entrada
-label.pack(pady=20)  # pady é a margem vertical
 
 # Endereço projeto
-btn_open_dialog = tk.Button(root, text="Selecionar Arquivo", command=open_file_dialog)
+btn_open_dialog = tk.Button(root, text="Selecionar diretóroi", command=open_file_dialog)
 btn_open_dialog.pack(pady=10)
 
-# Botão para fechar a janela
+# Botão para adicionar uma nova linha
+botao_adicionar_linha = tk.Button(root, text="Adicionar Linha", command=adicionar_linha)
+botao_adicionar_linha.pack(pady=10)
+
+# Botão para criar
 criarBtn = tk.Button(root, text="Crair projeto", command=Criar)
 criarBtn.pack()
 
