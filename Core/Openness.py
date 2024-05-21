f09711629b68ff856f43d293533f2d2973b9ed3b
dankocaboski <caboskidanko@gmail.com>
2024-05-21 15:40:10 -0300

    Feat: criarBtn trigger Openness.create_project()

--- Core/Openness.py
+++ Core/Openness.py
@@ -7,11 +7,12 @@ clr.AddReference(r'C:\Program Files\Siemens\Automation\Portal V15_1\PublicAPI\V1
 import Siemens.Engineering as tia
 
 project_dir = 'C:\\Users\\Willian\\Desktop\\tia_python'
-project_path = DirectoryInfo (project_dir)
 
-project_name = 'PythonTest'
 
 def create_project(project_path, project_name):
+    
+    project_path = DirectoryInfo (project_dir)
+    
     #Starting TIA
     print ('Starting TIA with UI')
     mytia = tia.TiaPortal(tia.TiaPortalMode.WithUserInterface)
--- Core/Screen.py
+++ Core/Screen.py
@@ -11,26 +11,33 @@ root = tk.Tk()
 root.geometry("600x400")
 root.title("MVP Tia Openness")
 
+# Variavel no nome do projeto
+project_name_var=tk.StringVar()
+
 ############### FUNÇÕES ################
 
 # Função para fechar a janela
-def fechar_janela():
+def Criar():
+    print("Caminho do arquivo: ", project_dir)
+    project_name = project_name_var.get()
+    print("Nome do projeto: ", project_name)
+    Openness.create_project(project_dir, project_name)
     root.destroy()
 
 # Caixa de dialogo
 def open_file_dialog():
-    global folder
-    folder = filedialog.askdirectory()
+    global project_dir
+    project_dir = filedialog.askdirectory()
         
 ############### Valoriaveis ################
 
-folder = None
+project_dir = None
 
 ############### CAMPOS ################
 
 # Nome do projeto
 label = tk.Label(root, text="Nome do projeto: ", font=("Arial", 24))
-entrada1 = tk.Entry(root)
+entrada1 = tk.Entry(root, textvariable = project_name_var)
 entrada1.pack()  # Adicione esta linha para exibir o campo de entrada
 label.pack(pady=20)  # pady é a margem vertical
 
@@ -39,8 +46,8 @@ btn_open_dialog = tk.Button(root, text="Selecionar Arquivo", command=open_file_d
 btn_open_dialog.pack(pady=10)
 
 # Botão para fechar a janela
-fechar_botao = tk.Button(root, text="Fechar", command=fechar_janela)
-fechar_botao.pack()
+criarBtn = tk.Button(root, text="Crair projeto", command=Criar)
+criarBtn.pack()
 
 # Executando o loop principal
 root.mainloop()
