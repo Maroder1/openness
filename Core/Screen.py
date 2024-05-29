40cc324ecffd8b8afd937c5e5e2c7c4fdcfcfc12
dankocaboski <caboskidanko@gmail.com>
2024-05-29 14:23:54 -0300

    Feat: Name and path validation

--- Core/Screen.py
+++ Core/Screen.py
@@ -17,20 +17,24 @@ root.title("RPA Tia Openness")
 # Variavel no nome do projeto
 project_name_var=tk.StringVar()
 
-############### FUNÇÕES ################
+############### FUNCTIONS ################
 
 # Função para fechar a janela
 def CreateProject():
     project_name = project_name_var.get()
     
-    devices = []
-    for linha in InfoHardware:
-        devices.append({"HardwareType": linha["combobox"].get(), "Mlfb":linha["mlfb"].get(), "Name": linha["entry"].get()})
-        
-    update_status("Creating project...")
-    Openness.create_project(project_dir, project_name, devices)
-    update_status(None)
-    # root.destroy()
+    if UserConfig.getDllPath(151):
+        label_status.config(text="Erro: Dll não configurada para esta versão do TIA")
+        return
+    
+    if project_name != None and project_name != '' and project_dir != None and project_dir != '': 
+        devices = []
+        for linha in InfoHardware:
+            devices.append({"HardwareType": linha["combobox"].get(), "Mlfb":linha["mlfb"].get(), "Name": linha["entry"].get()})   
+        Openness.create_project(project_dir, project_name, devices)
+    
+    else:
+        label_status.config(text="Erro: Nome do projeto ou diretório não informados")
 
 def open_directory_dialog():
     global project_dir
@@ -101,7 +105,7 @@ def slice_tupla(string):
     else:
         return string
         
-############### Valoriaveis ################
+############### VARIABLES ################
 
 project_dir = None
 NHardware = 0
@@ -117,7 +121,7 @@ mlfb_npde = []
 
 mlfb_List=[mlfb_Plc, mlfb_ihm, mlfb_npde]
 
-############### TELA ################
+############### SCREEN ################
 def main_screen():
     global screen_frames
     
@@ -136,7 +140,7 @@ def main_screen():
         user_config = ttk.Frame(root)
         screen_frames.append(user_config)
         
-        # Botão Configurações
+        # Button Configurações
         BtnUserSettings = tk.Button(user_config, text="...", command=user_config_screen)
         BtnUserSettings.pack(padx=5, pady=5, anchor='w')
         screen_frames.append(BtnUserSettings)
@@ -146,24 +150,24 @@ def main_screen():
         proj_config_frame = ttk.Frame(root)
         screen_frames.append(proj_config_frame)
         
-        # Nome do projeto
+        # Project name
         ProjectName = tk.Label(proj_config_frame, text="Nome do projeto: ")
         ProjectName.grid(row=0, column=0, padx=5, pady=5)
 
         entrada1 = tk.Entry(proj_config_frame, textvariable = project_name_var)
         entrada1.grid(row=0, column=1, padx=5, pady=5)
 
-        # Endereço projeto
+        # Path
         btn_open_dialog = tk.Button(proj_config_frame, text="Selecionar diretório", command=open_directory_dialog)
         btn_open_dialog.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
 
-        # Botão para criar
+        # Button para criar
         criarBtn = tk.Button(proj_config_frame, text="Crair projeto", command=CreateProject)
         criarBtn.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
 
         proj_config_frame.pack()
 
-        # Botão para adicionar uma nova linha
+        # Button para adicionar uma nova linha
         botao_adicionar_linha = tk.Button(root, text="Adicionar hardware", command=AddHardware)
         botao_adicionar_linha.pack(pady=10)
         screen_frames.append(botao_adicionar_linha)
@@ -173,6 +177,7 @@ def main_screen():
         screen_frames.append(hardwareConfig)
         
         global RAP_status_Tela
+        global label_status
         label_status = tk.Label(root, text="Nome do projeto: " + RAP_status_Tela)
         label_status.pack(padx=5, pady=5)
 
@@ -190,43 +195,43 @@ def user_config_screen():
     nova_label.pack()
     
     dll_config_frame = ttk.Frame(usr_config_screen)
+    
+    InstructionsDllPath = tk.Label(dll_config_frame, text="Indique o caminho das dlls:")
+    InstructionsDllPath.grid(row=0, column=0, padx=5, pady=5)
 
     dll_matrix = []
     
     def setDllTuple(Tia_Version):
         
         info_dll = {"Tia_Version": Tia_Version, "Path": open_file_dialog()}
-        print(info_dll)
         dll_matrix.append(info_dll)
     
     # Tia V15.1
     label151 = tk.Label(dll_config_frame, text="Tia V15.1:")
-    label151.grid(row=0, column=0, padx=5, pady=5)
+    label151.grid(row=1, column=0, padx=5, pady=5)
     
-    Btn151 = tk.Button(dll_config_frame, command=lambda: setDllTuple(151))
-    Btn151.grid(row=0, column=1, padx=5, pady=5)    
+    Btn151 = tk.Button(dll_config_frame, command=lambda: setDllTuple(151), width=10)
+    Btn151.grid(row=1, column=1, padx=5, pady=5)    
     
     # Tia V16
     label16 = tk.Label(dll_config_frame, text="Tia V16:")
-    label16.grid(row=1, column=0, padx=5, pady=5)
+    label16.grid(row=2, column=0, padx=5, pady=5)
     
-    Btn16 = tk.Button(dll_config_frame, command=lambda: setDllTuple(16))
-    Btn16.grid(row=1, column=1, padx=5, pady=5)
+    Btn16 = tk.Button(dll_config_frame, command=lambda: setDllTuple(16), width=10)
+    Btn16.grid(row=2, column=1, padx=5, pady=5)
     
     # Tia V17
     label17 = tk.Label(dll_config_frame, text="Tia V17:")
-    label17.grid(row=2, column=0, padx=5, pady=5)
+    label17.grid(row=3, column=0, padx=5, pady=5)
     
-    Btn17 = tk.Button(dll_config_frame, command=lambda: setDllTuple(17))
-    Btn17.grid(row=2, column=1, padx=5, pady=5,)
-    
-    SaveConfirmations = tk.Button(dll_config_frame, text="Salvar", command=lambda: setDllPath(dll_matrix))
-    SaveConfirmations.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
+    Btn17 = tk.Button(dll_config_frame, command=lambda: setDllTuple(17), width=10)
+    Btn17.grid(row=3, column=1, padx=5, pady=5,)
     
     dll_config_frame.pack(padx=5, pady=5)
     
+    SaveConfirmations = tk.Button(usr_config_screen, text="Salvar", command=lambda: setDllPath(dll_matrix))
+    SaveConfirmations.pack(padx=5, pady=5)
     
-    # Define um botão para fechar a nova janela
     fechar_botao = tk.Button(usr_config_screen, text="Fechar", command=usr_config_screen.destroy)
     fechar_botao.pack()
         
