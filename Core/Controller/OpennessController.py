035cc4e5e46ecb62b2419557db4316dc2baa87d3
dankocaboski <caboskidanko@gmail.com>
2024-06-03 14:22:05 -0300

    moving functions from OpennessController to OpennessService

--- Core/Controller/OpennessController.py
+++ Core/Controller/OpennessController.py
@@ -1,61 +1,24 @@
-import os
-import shutil
-import clr
-from repositories import UserConfig
 from Services import OpennessService
-from System.IO import DirectoryInfo
-
-def add_DLL(tia_Version):
-    try:
-        tuple = UserConfig.getDllPath(tia_Version)
-        project_dll = r'' + tuple[0]
-        clr.AddReference(project_dll)
-        
-        global tia
-        global hwf
-        global comp
-
-        import Siemens.Engineering as tia
-        import Siemens.Engineering.HW.Features as hwf
-        import Siemens.Engineering.Compiler as comp
-
-        RPA_status = 'DLL reference added successfully!'
-        print(RPA_status)
-
-    except Exception as e:
-        print ("Error adding DLL reference: ")
-        RPA_status = str(e)
-        print(RPA_status)
-        
-if UserConfig.CheckDll(151):
-    add_DLL(151)
-
-project_dir = ""
-mytia = None
 
 RPA_status = ""
 
 def create_project(project_path, project_name, hardware):
-    
-    global project_dir
+
     project_dir = OpennessService.set_project_dir(project_path)
     
     try:
-        project_path = DirectoryInfo (project_dir)
         
-        #Starting TIA
         global RPA_status
         RPA_status = 'Starting TIA UI'
         print(RPA_status)
         
-        global mytia
-        mytia = tia.TiaPortal(tia.TiaPortalMode.WithUserInterface)
+        mytia = OpennessService.create_project()
 
         #Creating new project
         RPA_status = 'Creating project'
         print(RPA_status)
         
-        myproject = mytia.Projects.Create(project_path, project_name)
+        myproject = mytia.Projects.Create(project_dir, project_name)
 
         deviceName = ''
         deviceMlfb = ''
@@ -68,12 +31,34 @@ def create_project(project_path, project_name, hardware):
             
         RPA_status = 'Project created successfully!'
         print(RPA_status)
-
+        
+        # open_project()
+        
         return
 
     except Exception as e:
         RPA_status = 'Error: ', e
         print(RPA_status)
+        return
+    
+def open_project():
+    try:
+        mokedPath = r'C:\Users\Willian\Documents\Automation\Factory_IO\PickNPlace_V15.1\PickNPlace_V15.1.ap15_1'
+        projeto = OpennessService.open_project(mokedPath)
+        export_Block(projeto)
+    except Exception as e:
+        RPA_status = 'Error opening project: ', e
+        print(RPA_status)
+        return
+    
+def export_Block(PlcSoftware):
+    RPA_status = 'Exporting block'
+    print(RPA_status)
+    try:
+        OpennessService.export_Fb(PlcSoftware)
+    except Exception as e:
+        RPA_status = 'Error exporting block: ', e
+        print('Error exporting block: ', e)
         return
         
     
--- Core/Services/OpennessService.py
+++ Core/Services/OpennessService.py
@@ -1,5 +1,47 @@
+import os
+import clr
+from System.IO import DirectoryInfo, FileInfo
+from repositories import UserConfig
+
+
+def add_DLL(tia_Version):
+    try:
+        tuple = UserConfig.getDllPath(tia_Version)
+        project_dll = r'' + tuple[0]
+        clr.AddReference(project_dll)
+        
+        global tia
+        global hwf
+        global comp
+
+        import Siemens.Engineering as tia
+        import Siemens.Engineering.HW.Features as hwf
+        import Siemens.Engineering.Compiler as comp
+
+        RPA_status = 'DLL reference added successfully!'
+        print(RPA_status)
+
+    except Exception as e:
+        print ("Error adding DLL reference: ")
+        RPA_status = str(e)
+        print(RPA_status)
+        
+if UserConfig.CheckDll(151):
+    add_DLL(151)
+    
+def create_project():
+    return tia.TiaPortal(tia.TiaPortalMode.WithUserInterface)
+
 def set_project_dir(path):
+    project_dir = configurePath(path)
+    return DirectoryInfo (project_dir)
+
+def configurePath(path):
     return path.replace("/", "\\")
+
+def open_project(project_path):
+    projects = tia.TiaPortal.Projects
+    return projects
 
 def addHardware(deviceType, deviceName, deviceMlfb, myproject):
     if deviceType == "PLC":
@@ -22,3 +64,22 @@ def addHardware(deviceType, deviceName, deviceMlfb, myproject):
     else:
         RPA_status = 'Unknown hardware type: ', deviceType
         print(RPA_status)
+        
+def export_Fb(PlcSoftware):
+    
+    Block = PlcSoftware.BlockGroup.Blocks.Find("MyBlock")
+    
+    diretorio_atual = os.path.dirname(os.path.abspath(__file__))
+
+    # Define o caminho da pasta "exportados"
+    caminho_pasta = os.path.join(diretorio_atual, 'exportados')
+
+    # Define o caminho do arquivo dentro da pasta "exportados"
+    caminho_arquivo = os.path.join(caminho_pasta, 'nome_do_arquivo.txt')
+    
+    with open(caminho_arquivo, 'w') as arquivo:
+        # Escreve alguma coisa no arquivo
+        arquivo.write(Block.Name)
+    
+    # Block.Export(new FileInfo(string.Format(@”D:\Samples\{0}.xml”, Block.Name)),
+    # # # ExportOptions.WithDefaults);
